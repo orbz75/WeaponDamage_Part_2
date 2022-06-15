9899fbdacbd7ed81b28b408f0247f22f409d03cd
orbz75 <orbz75@live.com>
2022-06-15 15:33:01 +0800

    Add project files.

---
+++ SwordDamage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponDamage_Part_2
+{
+    internal class SwordDamage : WeaponDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public SwordDamage(int startingRoll) : base(startingRoll) { }
+
+        protected override void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
+        }
+    }
+}
